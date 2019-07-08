830f9079f046d5adea0458ed1de8629d09af948a
CarlosVazPI <carlos.vazpi@gmail.com>
2019-07-09 00:15:17 +0100

    Added index

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+	compact: require('./compact'),
+	constant: require('./constant'),
+	every: require('./every'),
+	filter: require('./filter'),
+	forEach: require('./forEach'),
+	fromArray: require('./fromArray'),
+	map: require('./map'),
+	nil: require('./nil'),
+	range: require('./range'),
+	reduce: require('./reduce'),
+	reject: require('./reject'),
+	slice: require('./slice'),
+	some: require('./some'),
+	takeWhile: require('./takeWhile'),
+	toArray: require('./toArray')
+}
